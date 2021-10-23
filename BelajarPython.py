6938aeabc46ed8397da79382fb97c47f9503413f
ravansaaa <ravansa04@mhs.pelitabangsa.ac.id>
2021-10-24 02:00:16 +0700

    menambahkan file python

---
+++ BelajarPython.py
@@ -0,0 +1,14 @@
+# Mengambil input
+namal = input("Siapa nama kamu: ")
+namap = input("Siapa nama panggilan kamu: ")
+umur = input("Berapa umur kamu: ")
+nim = input("Berapa nim kamu: ")
+tempat = input("Kamu lahir dimana: ")
+alamat = input("Kamu tinggal dimana: ")
+tlp = input("Berapa nomor telepon kamu: ")
+# Menampilkan output
+print ("Assalamualaikum")
+print ("Let me introduce my self. My name is" ,namal, "but you can call me",namap,"My NIM is",nim,
+"I was born in",tempat,"and I am" ,umur, "years old.""I am very glad if you want to invite my house in",alamat,
+". So, don't forget to call me before with the number",tlp,)
+print ("Thank you.")
